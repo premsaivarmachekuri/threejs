271664b2705e9ca2499bc617c383c8a1cc9a0ff5
Premsai Vamra <premsaivarmacoding@gmail.com>
2023-03-18 22:43:30 +0530

    this file contains 2 cubes which need to be animated

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 
-//import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
-
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-// scene 
+
 const scene = new THREE.Scene();
-// camera
 const camera = new THREE.PerspectiveCamera(
-    75, window.innerWidth/window.innerHeight,
-    0.1,1000
-)
-//const orbit = new OrbitControls(camera, renderer.domElement);
-// Cube 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color:0x346beb});
-const cube = new THREE.Mesh(geometry, material);
-cube.position.set(1,1,1)
-scene.add(cube);
+  75, window.innerWidth / window.innerHeight, 0.1, 1000
+);
+
+function addCube(x, y, z) {
+  this.val1 = x;
+  this.val2 = y;
+  this.val3 = z;
+  this.geometry = new THREE.BoxGeometry(1, 1, 1);
+  this.material = new THREE.MeshBasicMaterial({ color: 0x346beb });
+  this.cube = new THREE.Mesh(this.geometry, this.material);
+  this.cube.position.set(this.val1, this.val2, this.val3);
+  scene.add(this.cube);
+}
+
+addCube.prototype.animate = function() {
+  this.cube.rotation.x += 0.01;
+  this.cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+};
+
+let cube1 = new addCube(2, 3, 1);
+let cube2 = new addCube(1, 1, 1);
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-camera.position.set(0,2,5);
-//orbit.update();
-function animate(){
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
-    renderer.render(scene,camera);
-}
+
+camera.position.set(0, 2, 5);
+
+renderer.setAnimationLoop(() => {
+  cube1.animate();
+  cube2.animate();
+});
+
 renderer.render(scene, camera);
-renderer.setAnimationLoop(animate);
